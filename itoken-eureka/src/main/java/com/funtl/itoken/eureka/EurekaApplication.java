dd2c6d1f5b16522cbad0ccf4e2981c1816dc88cf
luowangchang <1648942381@qq.com>
2019-03-05 10:18:37 +0800

    加入依赖部署项目

--- itoken-eureka/src/main/java/com/funtl/itoken/eureka/EurekaApplication.java
+++ itoken-eureka/src/main/java/com/funtl/itoken/eureka/EurekaApplication.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class,args);
     }
 }
